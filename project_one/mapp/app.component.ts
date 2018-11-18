8d435ead780a243eaf7ab577216789dca6ad93a6
huynvn010193 <hpn010193@gmail.com>
2018-11-18 18:46:32 +0700

    Commit edit project_one

---
+++ project_one/mapp/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'first-app',
+	// template: 'abc',
+	templateUrl: 'template/app.template.html',
+	styles: [``],
+	styleUrls: [`template/app.style.css`]
+})
+
+export class AppComponent { }
