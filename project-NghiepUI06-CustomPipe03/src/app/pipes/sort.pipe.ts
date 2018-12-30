31fcda8bc62af8e8cfb1b258bd0fa530fdecf020
huynvn010193 <hpn010193@gmail.com>
2018-12-30 22:00:22 +0700

    Done clip 24: lodash

--- project-NghiepUI06-CustomPipe03/src/app/pipes/sort.pipe.ts
+++ project-NghiepUI06-CustomPipe03/src/app/pipes/sort.pipe.ts
@@ -1,24 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import 'lodash';
+declare var _;
 
 @Pipe({
   name: 'sort'
 })
 export class SortPipe implements PipeTransform {
   transform(products: any[], sortBy : string, sortValue : number): any {
-    console.log(sortBy,'-',sortValue);
     if(sortBy === 'name'){
-      products.sort((a,b) => {
-        if(a.name > b.name) return sortValue;
-        else if(a.name < b.name) return -sortValue;
-        else return 0;
-      })
+      // products.sort((a,b) => {
+      //   if(a.name > b.name) return sortValue;
+      //   else if(a.name < b.name) return -sortValue;
+      //   else return 0;
+      // })
+      let temp = sortValue === 1 ? 'asc':'desc';
+      products = _.orderBy(products,['name'],[temp]);
     }
     if(sortBy === 'price'){
-      products.sort((a,b) => {
-        if(a.price > b.price) return sortValue;
-        else if(a.price < b.price) return -sortValue;
-        else return 0;
-      })
+      // products.sort((a,b) => {
+      //   if(a.price > b.price) return sortValue;
+      //   else if(a.price < b.price) return -sortValue;
+      //   else return 0;
+      // })
+      let temp = sortValue === 1 ? 'asc':'desc';
+      products = _.orderBy(products,['price'],[temp]);
     }
     return products;
   }
