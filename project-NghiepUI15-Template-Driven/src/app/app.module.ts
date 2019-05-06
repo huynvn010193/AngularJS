d7c45d18cf0327de2f0796598eb70016ba5074d7
huynvn010193 <hpn010193@gmail.com>
2019-05-06 23:01:23 +0700

    Done clip 48: Reactive Forms

--- project-NghiepUI15-Template-Driven/src/app/app.module.ts
+++ project-NghiepUI15-Template-Driven/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
+import { DataDrivenFormComponent } from './components/data-driven-form/data-driven-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent
+    TemplateFormComponent,
+    DataDrivenFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project-NghiepUI15-Template-Driven/src/app/components/data-driven-form/data-driven-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-driven-form',
+  templateUrl: './data-driven-form.component.html',
+  styleUrls: ['./data-driven-form.component.css']
+})
+export class DataDrivenFormComponent implements OnInit {
+  public frmUser : FormGroup;
+
+  constructor(
+    private _formBuilder : FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.frmUser = this._formBuilder.group({
+      username : ['',[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]],
+      password : ['',[Validators.required]],
+      fullname : [''],
+      email : ['',Validators.pattern('^[a-z][a-z0-9.-_]{1,}@[a-z0-9.-]{2,}[.]{1}[a-zA-Z]{2,}')],
+      phone : ['']
+    });
+
+    // this.frmUser.valueChanges.subscribe(data => {
+    //   console.log('data-valueChanges',data);
+    // })
+  }
+
+  onSubmitForm() {
+    console.log('this.frmUser.value',this.frmUser);
+  }
+
+  onResetForm() {
+    this.frmUser.reset();
+  }
+}
